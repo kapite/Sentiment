1ebe1252f219addea7ad522f3719326baf71cc53
Karl Piteira <kpiteira@hotmail.com>
2017-11-14 14:51:47 -0800

    Added Sentiment code

--- Sentiment/AppDelegate.swift
+++ Sentiment/AppDelegate.swift
@@ -12,35 +12,55 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        SentimentClient.shared.apiKey = ""
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
+    
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        SentimentClient.shared.obtainKey(){keyResponse in
+            if let document = keyResponse {
+                SentimentClient.shared.apiKey = document.key
+                SentimentClient.shared.region = document.region
+            } else {
+                self.showApiKeyAlert(application)
+            }
+        }
+        
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    func showApiKeyAlert(_ application: UIApplication) {
+        
+        if SentimentClient.shared.apiKey == nil || SentimentClient.shared.apiKey!.isEmpty {
+            
+            let alertController = UIAlertController(title: "Configure App", message: "Enter a Text Analytics API Subscription Key. Or add the key in code in `didFinishLaunchingWithOptions`", preferredStyle: .alert)
+            
+            alertController.addTextField() { textField in
+                textField.placeholder = "Subscription Key"
+                textField.returnKeyType = .done
+            }
+            
+            alertController.addAction(UIAlertAction(title: "Get Key", style: .default) { a in
+                if let getKeyUrl = URL(string: "https://portal.azure.com/#blade/HubsExtension/BrowseResourceBlade/resourceType/Microsoft.CognitiveServices%2Faccounts") {
+                    UIApplication.shared.open(getKeyUrl, options: [:]) { opened in
+                        print("Opened GetKey url successfully: \(opened)")
+                    }
+                }
+            })
+            
+            alertController.addAction(UIAlertAction(title: "Done", style: .default) { a in
+                if alertController.textFields?.first?.text == nil || alertController.textFields!.first!.text!.isEmpty {
+                    self.showApiKeyAlert(application)
+                } else {
+                    SentimentClient.shared.apiKey = alertController.textFields!.first!.text
+                }
+            })
+            
+            window?.rootViewController?.present(alertController, animated: true) { }
+        }
     }
-
-
 }
 
--- Sentiment/ViewController.swift
+++ Sentiment/ViewController.swift
@@ -10,16 +10,118 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sentimentTextView: UITextView!
+    @IBOutlet weak var sentimentTextContainer: UIView!
+    @IBOutlet weak var sentimentTextPlaceholder: UILabel!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var activityLabel: UILabel!
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    
+    let submit = "Submit"
+    let reset = "Reset"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupBorders()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func submitButtonTouchUpInside(_ sender: Any) {
+        
+        if submitButton.currentTitle == submit {
+            
+            if !sentimentTextView.hasText {
+                showErrorAlert("Hold up", message: "Gotta add some text first.", dismiss: "Got it"); return
+            }
+            
+            setActivity(true)
+            
+            SentimentClient.shared.determineSentiment(sentimentTextView.text) { sentimentResponse in
+                
+                self.setActivity(false)
+                
+                if let response = sentimentResponse, let document = response.documents.first {
+                    
+                    self.setSentiment(Sentiment.getSentiment(document.score))
+                    
+                } else {
+                    
+                    self.showErrorAlert("Error \(sentimentResponse?.errorStatusCode ?? -1)", message: sentimentResponse?.errorMessage ?? "", dismiss: "Okay")
+                }
+            }
+            
+        } else if submitButton.currentTitle == reset {
+            
+            setSentiment(nil)
+            
+            resetTextView()
+        }
     }
-
-
+    
+    
+    func showErrorAlert(_ title:String, message:String, dismiss:String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: dismiss, style: .default) { a in
+            self.dismiss(animated: true) { }
+        });
+        
+        present(alertController, animated: true) {}
+    }
+    
+    
+    func setSentiment(_ sentiment:Sentiment?) {
+        emojiLabel.text = sentiment?.emoji
+        view.backgroundColor = sentiment?.color ?? #colorLiteral(red: 1, green: 0.4352941176, blue: 0.4117647059, alpha: 1)
+        submitButton.setTitle(sentiment == nil ? submit : reset, for: .normal)
+    }
+    
+    
+    func resetTextView() {
+        sentimentTextView.text = nil
+        sentimentTextPlaceholder.isHidden = false
+        sentimentTextView.isEditable = true
+    }
+    
+    
+    func setActivity(_ activity: Bool) {
+        activityLabel.isHidden = !activity
+        activityIndicator.isHidden = !activity
+        sentimentTextView.isEditable = !activity
+        submitButton.isUserInteractionEnabled = !activity
+        
+        if activity && !activityIndicator.isAnimating {
+            activityIndicator.startAnimating()
+        }
+    }
+    
+    
+    func setupBorders() {
+        sentimentTextContainer.layer.borderWidth = 1
+        sentimentTextContainer.layer.borderColor = #colorLiteral(red: 0.6078431373, green: 0.6078431373, blue: 0.6078431373, alpha: 1)
+        sentimentTextContainer.layer.cornerRadius = 8
+        
+        submitButton.layer.borderWidth = 1.5
+        submitButton.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
+    // MARK - UITextViewDelegate
+    
+    func textViewDidChange(_ textView: UITextView) {
+        sentimentTextPlaceholder.isHidden = textView.hasText
+    }
 }
 
